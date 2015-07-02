dc5b326dc2cef088723a95a2340a3677af7d9023
Russ Hewson <russ.hewson@gmail.com>
2015-07-02 18:27:45 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+BotFA
+=====
+
+A Symfony project created on July 2, 2015, 4:00 pm.
---
+++ src/Meldon/BotFABundle/MeldonBotFABundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Meldon\BotFABundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class MeldonBotFABundle extends Bundle
+{
+}
